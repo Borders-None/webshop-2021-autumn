027e89e0614071c286e5f86c6605e0bf0e944e6b
Josip Grgić <33314617+josipgrgic@users.noreply.github.com>
2022-03-06 15:21:11 +0100

    API implemented, documentation written

--- README.md
+++ README.md
@@ -5,3 +5,35 @@ To run the REST API:
  - Navigate to the `/api` folder
  - Run `npm install` to install all the dependencies
  - `node index.js` to run it
+
+API endpoints:
+ - `GET /api/shoes`
+   - Returns a list of shoes based on filter criteria
+   - It is possible to pass a filter object in the body of the request that will be used to filter shoes
+
+``` js
+{
+   "maxPrice": 50, // The maximum price a shoe has to have
+   "minPrice": 40, // The minimum price of a shoe
+   "search": "Natural", // Search term, searches on title and brand properties
+   "pageSize": 10 // The number of shoes that the current page of results will contain
+   "pageNumber": 5, // The page index, numeration starts with 1
+   "categories": [2, 4], // The categories of the shoes that have to be included in teh result
+
+}
+```
+ - `GET /api/shoes/id`
+   - Get details about a single shoe
+
+ - `GET /api/categories`
+   - Returns all categories the database has
+   - They are in a flat tree structure, for example:
+     -  Shoes -> Boys' Shoes -> Boots are all the shoes that are boys' boots.
+     -  The dictionary would show that relationship as 1 -> 2 -> 45
+     -  If we were to search for all boys' shoes, we would get all the boys's boots including boy's sandals
+
+ - `POST /api/purchase`
+   - Creates a purchase
+   - The body has to contain a `shoes` array of shoe ids to purchase
+  
+The files `shoes.js` and `categories.js` contain the website data. Feel free to explore the files to get a grip on what data is provided. For especially brave readers, you can even take a look on `index.js` and see how the REST API is implemented.
--- api/index.js
+++ api/index.js
@@ -1,32 +1,173 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import multer from 'multer'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import multer from 'multer';
+import cors from 'cors';
+import { shoes } from './shoes.js';
+import { categories } from './categories.js';
 
-const port = 3000
+const port = 3000;
 
 // Set up the express app
-const app = express()
+const app = express();
 
-const upload = multer()
+const upload = multer();
 
 // for parsing application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // for parsing multipart/form-data
-app.use(upload.array())
-app.use(express.static('public'))
+app.use(upload.array());
+app.use(express.static('public'));
 
 // Set up CORS
-app.use(cors())
+app.use(cors());
+
+app.get('/api/shoes', async (req, res) => {
+  try {
+    console.log(req.body);
+
+    // validate search object
+    if (req.body?.categories?.length) {
+      if (!req.body.categories.every((c) => !!categories[c])) {
+        return res.status(400).send({
+          message: 'Nonexistent category provided!',
+        });
+      }
+    }
+
+    if (!(!!req.body?.pageSize === !!req.body?.pageNumber)) {
+      return res.status(400).send({
+        message:
+          'Page size and page number parameters have to be either all defined or all undefined!',
+      });
+    }
+
+    const filteredCategories = [];
+    if (req.body?.categories?.length) {
+      filteredCategories.push(...req.body.categories);
+
+      for (let i = 0; i < filteredCategories.length; i++) {
+        const categoryId = filteredCategories[i];
+        if (categories[categoryId].subCategories?.length) {
+          filteredCategories.push(...categories[categoryId].subCategories);
+        }
+      }
+    }
+
+    let result = shoes.filter((s) => {
+      if (!req.body) {
+        return true;
+      }
+
+      if (req.body.search) {
+        const doesTitleContainSearchExpression = s.title
+          .toLowerCase()
+          .includes(req.body.search.toLowerCase());
+
+        const doesBrandContainSearchExpression = s.brand
+          .toLowerCase()
+          .includes(req.body.search.toLowerCase());
+
+        if (
+          !doesBrandContainSearchExpression &&
+          !doesTitleContainSearchExpression
+        ) {
+          return false;
+        }
+      }
+
+      if (req.body.minPrice && s.price < req.body.minPrice) {
+        return false;
+      }
+
+      if (req.body.maxPrice && s.price > req.body.maxPrice) {
+        return false;
+      }
+
+      if (
+        filteredCategories.length &&
+        !filteredCategories.includes(s.categoryId)
+      ) {
+        return false;
+      }
+
+      return true;
+    });
+
+    const totalResults = result.length;
+
+    if (req.body?.pageSize && req.body?.pageNumber) {
+      const startIndex = (req.body.pageNumber - 1) * req.body.pageSize;
+      const endIndex = startIndex + req.body.pageSize;
+
+      if (startIndex > result.length - 1) {
+        return res.status(400).send({
+          message: 'Page out of bounds',
+        });
+      }
+
+      if (endIndex > result.length - 1) {
+        endIndex = result.length - 1;
+      }
+
+      result = result.slice(startIndex, endIndex);
+    }
+
+    const mappedResult = result.map((s) => {
+      return {
+        id: s.id,
+        title: s.title,
+        price: s.price,
+        categoryId: s.categoryId,
+        imageUrl: s.images.length ? s.images[0] : null,
+      };
+    });
+
+    await new Promise((r) => setTimeout(r, 2000));
+
+    return res.status(201).send({ totalResults, result: mappedResult });
+  } catch (e) {
+    return res.status(500).send({ e });
+  }
+});
+
+app.get('/api/shoes/:id/', async (req, res) => {
+  await new Promise((r) => setTimeout(r, 2000));
+
+  const shoe = shoes.find((x) => x.id === +req.params.id);
+
+  if (shoe) {
+    return res.status(200).send(shoe);
+  }
+
+  return res.status(404).send({
+    message: `No shoe with the id ${req.params.id} found.`,
+  });
+});
+
+app.get('/api/categories', async (req, res) => {
+  await new Promise((r) => setTimeout(r, 2000));
+  return res.status(200).send(categories);
+});
 
 app.use(function (req, res) {
-  res.send(404)
-})
+  res.send(404);
+});
+
+app.post('/api/purchase', async (req, res) => {
+  if (!req.body?.shoes.length) {
+    return res.status(400).send({
+      message: 'No shoes were provided',
+    });
+  }
+  await new Promise((r) => setTimeout(r, 2000));
+
+  return res.status(201);
+});
 
 app.listen(port, () => {
-  console.log(`Email app listening at http://localhost:${port}`)
-})
+  console.log(`Email app listening at http://localhost:${port}`);
+});
