146093fe3db2c7760c86c3ed59e717c82bb20518
Fatma <shauqifatma@gmail.com>
2022-03-31 18:00:10 +0200

    make the header mobile responsive

--- index.js
+++ index.js
@@ -7,10 +7,3 @@ async function web() {
   let data = await response.json();
   console.log(data);
 }
-
-const hamburger = document.getElementById('hamburger');
-hamburger.addEventListener('click', function (c) {
-  const ul = document.querySelector('nav');
-  ul.classList.toggle('meanu');
-  hamburger.classList.toggle('cross');
-});
